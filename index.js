7066c62acb712e00030a0fba4e6921f8f73d758a
harshitnitjsr <harshit26092004@gmail.com>
2025-02-14 02:42:54 +0530

    minor fixes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-require("dotenv").config();
-const uploadRoutes = require("./routes/uploadRoutes");
-const streamRoutes = require("./routes/streamRoutes");
-const userRoutes = require("./routes/authRoutes");
-const mediaRoutes = require("./routes/mediaRoutes");
-const connectDB = require("./config/mongoDb");
-const app = express();
+const express = require("express")
+const cors = require("cors")
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+require("dotenv").config()
+const uploadRoutes = require("./routes/uploadRoutes")
+const streamRoutes = require("./routes/streamRoutes")
+const userRoutes = require("./routes/authRoutes")
+const mediaRoutes = require("./routes/mediaRoutes")
+const connectDB = require("./config/mongoDb")
+const app = express()
 app.use(
   cors({
     origin: [
@@ -18,9 +18,9 @@ app.use(
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
-);
-app.use(express.json());
-connectDB();
+)
+app.use(express.json())
+connectDB()
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -32,17 +32,17 @@ const swaggerOptions = {
     servers: [{ url: "https://clinikk-project-production.up.railway.app" }],
   },
   apis: ["./docs/swagger.yaml"],
-};
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/upload", uploadRoutes);
-app.use("/stream", streamRoutes);
-app.use("/auth", userRoutes);
-app.use("/media", mediaRoutes);
-const PORT = process.env.PORT || 5000;
+}
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use("/upload", uploadRoutes)
+app.use("/stream", streamRoutes)
+app.use("/auth", userRoutes)
+app.use("/media", mediaRoutes)
+const PORT = process.env.PORT || 5000
 if (require.main === module) {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
+    console.log(`🚀 Server running on port ${PORT}`)
+  })
 }
 module.exports = app;
